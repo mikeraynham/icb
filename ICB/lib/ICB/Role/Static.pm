4d133427e99b553ad6fdf951c676e05692fcfb68
Mike Raynham <enquiries@mikeraynham.co.uk>
2012-06-09 22:39:50 +0100

    Moved common code to ICB::Role::Static

--- ICB/lib/ICB/Role/Static.pm
+++ ICB/lib/ICB/Role/Static.pm
@@ -4,10 +4,37 @@ use strict;
 use warnings;
 
 use Moose::Role;
+use MooseX::Types::Path::Class;
 
 requires qw/
     _content_type
 /;
+
+has minifier   => (
+    is         => 'ro',
+    lazy_build => 1,
+    traits     => ['Code'],
+    handles    => { minify  => 'execute' },
+);
+
+has date_stamp => (
+    is         => 'ro',
+    isa        => 'Int',
+    required   => 1,
+);
+
+has time_stamp => (
+    is         => 'ro',
+    isa        => 'Int',
+    required   => 1,
+);
+
+has path       => (
+    is         => 'ro',
+    isa        => 'Path::Class::File',
+    required   => 1,
+    coerce     => 1,
+);
 
 sub handles_content_type {
     my $self         = shift;
--- ICB/lib/ICB/Static/CSS.pm
+++ ICB/lib/ICB/Static/CSS.pm
@@ -7,37 +7,10 @@ use autodie qw/ :all /;
 use Carp;
 
 use Moose;
-use MooseX::Types::Path::Class;
 
 with qw/
     ICB::Role::Static
 /;
-
-has minifier   => (
-    is         => 'ro',
-    lazy_build => 1,
-    traits     => ['Code'],
-    handles    => { minify  => 'execute' },
-);
-
-has date_stamp => (
-    is         => 'ro',
-    isa        => 'Int',
-    required   => 1,
-);
-
-has time_stamp => (
-    is         => 'ro',
-    isa        => 'Int',
-    required   => 1,
-);
-
-has path       => (
-    is         => 'ro',
-    isa        => 'Path::Class::File',
-    required   => 1,
-    coerce     => 1,
-);
 
 sub _build_minifier {
     require CSS::Minifier::XS;
