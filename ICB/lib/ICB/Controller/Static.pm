42b7849dd0a1fca5809b5da7928b33fb9cc9ff7c
Mike Raynham <enquiries@mikeraynham.co.uk>
2012-06-10 12:50:43 +0100

    Added base path to ICB::Controller::Static to allow each content type to be categorised.

--- ICB/lib/ICB/Controller/Static.pm
+++ ICB/lib/ICB/Controller/Static.pm
@@ -32,19 +32,38 @@ chain_point '_static'
     => sub {
     my $self = shift;
     my $c    = shift;
-
+    
     $c->stash( content_type => $c->request->preferred_content_type );
     $c->response->content_type( $c->stash->{content_type} );
 };
 
-chain_point '_date'
+chain_point '_base'
     => chained '_static'
     => path_part ''
     => capture_args 1
     => sub {
     my $self = shift;
     my $c    = shift;
+    my $base = shift;
+
+    $c->log->debug( "_base: $base" );
+
+    $c->empty_detach( 404 )
+        unless ( $base =~ /^[a-z]+$/ );
+
+    $c->stash( base => $base );
+};
+
+chain_point '_date'
+    => chained '_base'
+    => path_part ''
+    => capture_args 1
+    => sub {
+    my $self = shift;
+    my $c    = shift;
     my $date = shift;
+
+    $c->log->debug( "_date: $date" );
 
     $c->empty_detach( 404 )
         unless ( $date =~ /^\d{8}$/ );
@@ -61,24 +80,28 @@ chain_point '_time'
     my $c    = shift;
     my $time = shift;
 
+    $c->log->debug( "_time: $time" );
+
     $c->empty_detach( 404 )
         unless ( $time =~ /^\d{6}$/ );
         
     $c->stash( time => $time );
 };
 
-get 'file'
+get 'path'
     => chained '_time'
     => path_part ''
     => sub {
     my $self = shift;
     my $c    = shift;
     my $file = file(
-        'static',
+        $c->stash->{base},
         $c->stash->{date},
         $c->stash->{time},
         @_
     );
+
+    $c->log->debug( '_path' );
 
     $c->log->debug( $c->stash->{content_type} );
     $c->log->debug( $file );
