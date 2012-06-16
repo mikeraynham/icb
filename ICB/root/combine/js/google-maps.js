906bae9412fe5daf75987f61ce83aa876140c152
Mike Raynham <enquiries@mikeraynham.co.uk>
2012-06-16 21:41:14 +0100

    Added JavaScript modules, renamed images to img.

---
+++ ICB/root/combine/js/google-maps.js
@@ -0,0 +1,28 @@
+function initialize() {
+    var myLatLng = new google.maps.LatLng(53.257725,-2.122331);
+
+    var myOptions = {
+        center: myLatLng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(
+        document.getElementById("map_canvas"),
+        myOptions
+    );
+
+    setTimeout(
+        function () {
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                title: 'Inca Café Bar',
+                map: map,
+                animation: google.maps.Animation.DROP
+            });
+        },
+        1500
+    );
+}
+
+window.onload = initialize;
